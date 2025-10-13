40ea2489ec9d3dd91ab44474903169ddb07f5b4a
LootDust <92685802+LootDust@users.noreply.github.com>
2025-10-13 10:51:56 +0800

    Make shout command more realistic

--- src/main/java/com/lootdust/simplegroupmsg/ShoutCommand.java
+++ src/main/java/com/lootdust/simplegroupmsg/ShoutCommand.java
@@ -25,7 +25,7 @@ public class ShoutCommand {
                 } else {
                     for (ServerPlayer hearer : context.getSource().getLevel().getPlayers(Predicates.alwaysTrue())) {
                         hearer.sendSystemMessage((Component.literal("[ Server: ")
-                                .append(Component.literal(MessageArgument.getMessage(context, "message").getString()).append(Component.literal(" ]")))).withStyle(ChatFormatting.GRAY).withStyle(ChatFormatting.ITALIC));
+                                .append(Component.literal(MessageArgument.getMessage(context, "message").getString()).append(Component.literal(" ]")))).withStyle(ChatFormatting.DARK_GRAY).withStyle(ChatFormatting.ITALIC));
                     }
                     return MessageArgument.getMessage(context, "message").getString().length();
                 }
